3b863ef0730f09bd76de35f8a83c8ca8fe6d6595
itacamba <itacamba@live.com>
2020-03-30 14:33:46 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index #This tells Sinatra to load a file called index.erb inside the directory "views"
+	end
+
+	get '/info' do 
+		erb :info
 	end
 end
